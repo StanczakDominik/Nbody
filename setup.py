c617db3f35181270863f5d902b38e800f8a56a59
Dominik Stańczak <stanczakdominik@gmail.com>
2018-10-28 16:10:43 +0100

    Setup.py basics

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NBody',
+    version='0.1.0',
+    packages=['nbody'],
+    url='https://github.com/StanczakDominik/Nbody',
+    license='CC BY 4.0',
+    author='Dominik Stańczak',
+    author_email='stanczakdominik@gmail.com',
+    description='n-body/molecular dynamics in Python on the GPU via numba.cuda'
+    )
