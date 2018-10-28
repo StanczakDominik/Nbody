d1095b594c6bb4fcc5f1499078aa6ac0f5e51fa9
Dominik Stańczak <stanczakdominik@gmail.com>
2018-10-28 16:50:48 +0100

    travis test matrix

--- nbody/forces/test_forces.py
+++ nbody/forces/test_forces.py
@@ -7,7 +7,9 @@ from nbody.forces import lenard_jones_force
 @pytest.mark.parametrize("r2, expected_force_on_1",
                          [
                              [1, 0],
-                             [2, 0.369140625],
+                             [2, 0.369140625],  # regression test   # TODO check signs
+                             [-1, 0],
+                             [-2, -0.369140625],  # regression test
                          ])
 def test_lenard_jones_1d(r2, expected_force_on_1):
     r1 = 0
