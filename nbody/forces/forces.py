d8108cfff0f81f8dbaf42afae6139d7b9e3a75f2
Dominik Stańczak <stanczakdominik@gmail.com>
2018-11-15 19:53:52 +0100

    fix sign

--- nbody/forces/forces.py
+++ nbody/forces/forces.py
@@ -3,5 +3,5 @@ import numba
 @numba.njit()
 def lenard_jones_force(r1, r2, epsilon=1, rm=1):
     r = r1 - r2
-    return epsilon * 12 * ((rm / r) ** 11 - (rm / r) ** 5 )
+    return - epsilon * 12 * ((rm / r) ** 11 - (rm / r) ** 5 )
 
--- nbody/forces/test_forces.py
+++ nbody/forces/test_forces.py
@@ -6,10 +6,12 @@ from nbody.forces import lenard_jones_force
 
 @pytest.mark.parametrize("r2, expected_force_on_1",
                          [
+                             [0.5, 24192],
                              [1, 0],
-                             [2, 0.369140625],  # regression test   # TODO check signs
+                             [2, -0.369140625],  # regression test
                              [-1, 0],
-                             [-2, -0.369140625],  # regression test
+                             [-2, 0.369140625],  # regression test
+                             [-0.5, -24192],
                          ])
 def test_lenard_jones_1d(r2, expected_force_on_1):
     r1 = 0
