33bd7ef56cf9b4475cf347ba605af82a48ddb9b5
Dominik Stańczak <stanczakdominik@gmail.com>
2018-12-05 20:28:58 +0100

    init_matrices

--- nbody/particles.py
+++ nbody/particles.py
@@ -2,12 +2,17 @@ from nbody import calculate_forces, calculate_movements, accelerate, move, save_
 import numpy as np
 import h5py
 
-def run(hdf5_file: h5py.File, N: int = int(1e4)):
+def initialize_matrices(json_data=None):
     m = np.ones(N, dtype=float)
+    q = np.ones(N, dtype=float)
     r = np.random.random((N, 3))
     v = np.random.random((N, 3))
     forces = np.empty_like(v)
     movements = np.empty_like(r)
+    return m, q, r, v, forces, movements
+
+def run(hdf5_file: h5py.File, N: int = int(1e4)):
+    m, q, r, v, forces, movements = initialize_matrices()
 
     for i_iteration in range(N_iterations):
         calculate_forces(forces, r, v, m)
