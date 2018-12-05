623498d4d9967522a7daf779f5508adb6825a0ac
Dominik Stańczak <stanczakdominik@gmail.com>
2018-12-05 20:22:07 +0100

    basic iteration loop

--- nbody/forces/forces.py
+++ nbody/forces/forces.py
@@ -1,7 +1,14 @@
+import math
 import numba
 
 @numba.njit()
 def lenard_jones_force(r1, r2, epsilon=1, rm=1):
     r = r1 - r2
     return - epsilon * 12 * ((rm / r) ** 11 - (rm / r) ** 5 )
+
+@numba.njit()
+def yukawa_force(r1, r2, rd = 1):
+    # https://en.wikipedia.org/wiki/Electric-field_screening
+    r = r1 - r2
+    raise NotImplementedError
 
---
+++ nbody/particles.py
@@ -0,0 +1,21 @@
+from nbody import calculate_forces, calculate_movements, accelerate, move, save_iteration
+import numpy as np
+import h5py
+
+def run(hdf5_file: h5py.File, N: int = int(1e4)):
+    m = np.ones(N, dtype=float)
+    r = np.random.random((N, 3))
+    v = np.random.random((N, 3))
+    forces = np.empty_like(v)
+    movements = np.empty_like(r)
+
+    for i_iteration in range(N_iterations):
+        calculate_forces(forces, r, v, m)
+        calculate_movements(movements, r, v)
+        accelerate(v, forces)
+        move(r, movements)
+
+        if check_saving_time(i_iteration):
+            save_iteration(i_iteration, hdf5_file, r, v)
+    hdf5_file.close()
+        
