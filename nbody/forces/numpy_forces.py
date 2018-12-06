ee56a638414cd2b481e517191a4a566a92bfbbf2
Dominik Stańczak <stanczakdominik@gmail.com>
2018-12-06 11:43:03 +0100

    numpy_forces

--- nbody/forces/numpy_forces.py
+++ nbody/forces/numpy_forces.py
@@ -1,16 +1,14 @@
-import math
-import numba
+import numpy as np
 
-@numba.njit()
 def lenard_jones_force(r1, r2, well_depth=1, diameter=1):
     r = r1 - r2
-    # TODO directions are going to break if doing it this way
-    return - 12 * well_depth *  ((diameter / r) ** 11 - (diameter / r) ** 5 )
+    norm_r = np.linalg.norm(r)
+    e_r = r / norm_r
+    return - 12 * e_r * well_depth *  ((diameter / norm_r) ** 11 - (diameter / norm_r) ** 5 )
 
-@numba.njit()
 def lenard_jones_potential(r1, r2, well_depth=1, diameter=1):
     r = r1 - r2
-    norm_r = (r[0] ** 2 + r[1] ** 2 + r[2] ** 2)**0.5
+    norm_r = np.linalg.norm(r)
     return 4 * well_depth * ((diameter / norm_r) ** 12 - (diameter / norm_r) ** 6 )
 
 @numba.njit()
