1fa305a056699c1127dd9d1bd9cc964d9ed7a18b
Dominik Stańczak <stanczakdominik@gmail.com>
2018-12-06 21:17:52 +0100

    LJ forces calculation via numpy

--- nbody/forces/numpy_forces.py
+++ nbody/forces/numpy_forces.py
@@ -1,13 +1,30 @@
 import numpy as np
 
-def lenard_jones_force(r1, r2, well_depth=1, diameter=1):
-    r = r1 - r2
-    norm_r = np.linalg.norm(r)
-    e_r = r / norm_r
-    return - 12 * e_r * well_depth *  ((diameter / norm_r) ** 11 - (diameter / norm_r) ** 5 )
+def lenard_jones_force(r, well_depth=1, diameter=1):
+    return - 12 * well_depth *  ((diameter / r) ** 11 - (diameter / r) ** 5 )
+
+def calculate_LJ_forces(r):
+    # 1. get a NxNx3 antisymmetric (upper triangular) matrix of vector distances
+    # 2a. from 1 get a normalized NxNx3 antisymmetric (matrix of direction vectors
+    # 2b. from 1 get a NxN (upper triangular due to symmetry) matrix of scalar distances
+    # 3b. get a NxN matrix of force magnitudes (reshapable to 
+    # 3. multiply 2a by 3b to get forces
+    # 4. update existing force matrix
+    # TODO optimize with upper triangular matrix
+    N = r.shape[0]
+    rij = r.reshape(N, 1, 3) - r.reshape(1, N, 3)
+    distances_ij = np.sqrt(np.sum(rij ** 2, axis=2, keepdims=True))
+    distances_ij[np.arange(N), np.arange(N), :] = np.inf
+    directions_ij = rij / distances_ij
+    forces = lenard_jones_force(distances_ij) * directions_ij
+    return forces.sum(axis=1)
 
 def lenard_jones_potential(r1, r2, well_depth=1, diameter=1):
     r = r1 - r2
     norm_r = np.linalg.norm(r)
     return 4 * well_depth * ((diameter / norm_r) ** 12 - (diameter / norm_r) ** 6 )
+
+def calculate_forces(forces, r, p, m):
+    forces[...] = calculate_LJ_forces(r)
+
 
--- nbody/run_nbody.py
+++ nbody/run_nbody.py
@@ -1,5 +1,5 @@
 # from nbody import calculate_forces, calculate_movements, accelerate, move, save_iteration
-from forces.numpy_forces import lenard_jones_force
+from forces import numpy_forces
 import numpy as np
 import h5py
 import click # TODO cmd-line interface
@@ -12,11 +12,16 @@ def move(r: np.array, p: np.array, m: np.array, dt: float, boundary_conditions: 
     if boundary_conditions=="periodic":
         raise NotImplementedError # TODO
 
+def initialize_zero_cm_momentum(p):
+    average_momentum = p.mean(axis=0)
+    p -= average_momentum
+
 def initialize_matrices(N, json_data=None):
     m = np.ones((N, 1), dtype=float)
     q = np.ones((N, 1), dtype=float)
     r = np.random.random((N, 3)) * 20
     p = np.random.normal(size=(N, 3))
+    initialize_zero_cm_momentum(p)
     forces = np.empty_like(p)
     movements = np.empty_like(r)
     dt = 0.001 # TODO load from json
@@ -29,15 +34,6 @@ def verlet_step(r, p, m, forces, dt):
     calculate_forces(forces, r, p, m)
     accelerate(p, forces, dt/2)
 
-def calculate_forces(forces, r, p, m):
-    N = m.size
-    forces.fill(0)
-    for i in range(N):
-        for j in range(i+1, N):
-            calculated_force = lenard_jones_force(r[i], r[j], well_depth=1, diameter=1)
-            forces[i] += calculated_force
-            forces[j] -= calculated_force
-
 def save_iteration(i_iteration, hdf5_file, r, p, m, q):
     print(i_iteration, r.mean(axis=0), p.mean(axis=0))
 
@@ -45,7 +41,7 @@ def check_saving_time(i_iteration):
     return (i_iteration % 10) == 0
 
 def run(hdf5_file: h5py.File = None,
-        N: int = int(1e3),
+        N: int = int(5e3),
         N_iterations = 100):
     # TODO use OpenPMD for saving instead of hdf5?
     m, q, r, p, forces, movements, dt = initialize_matrices(N)
