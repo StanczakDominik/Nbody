84980212fbdb13a5b9c0101e7363fb6fac3b13e6
Dominik Stańczak <stanczakdominik@gmail.com>
2018-12-19 15:44:46 +0100

    fix verlet reversibility test

--- nbody/tests/test_integrators.py
+++ nbody/tests/test_integrators.py
@@ -9,25 +9,32 @@ from nbody.integrators import verlet_step
 
 
 N = 3
-N_iterations = 5
+N_iterations = 50
+# dt = 1e-3
 @given(
     r=arrays(np.float,
              (N, 3),
-             floats(allow_infinity=False,
+             floats(min_value = -1e6,
+                    max_value = 1e6,
+                    allow_infinity=False,
                     allow_nan=False,
                     ),
-             unique=True
+             unique=True,
              ),
-    p=arrays(np.float,
+    v=arrays(np.float,
              (N, 3),
-             floats(allow_infinity=False,
+             floats(min_value = -1e6,
+                    max_value = 1e6,
+                    allow_infinity=False,
                     allow_nan=False)),
     dt=floats(min_value=1e-3,
+              max_value=2,
               allow_nan=False,
               allow_infinity=False),
     )
-def test_verlet_integrator_reversible(r, p, dt):
+def test_verlet_integrator_reversible(r, v, dt):
     m = np.ones((r.shape[0], 1))
+    p = m * v
     forces = calculate_forces(r, m = m)
     r_init = r.copy()
     p_init = p.copy()
@@ -35,6 +42,6 @@ def test_verlet_integrator_reversible(r, p, dt):
         verlet_step(r, p, m, forces, dt, calculate_forces)
     for i in range(N_iterations):
         verlet_step(r, p, m, forces, -dt, calculate_forces)
-    np.testing.assert_allclose(r, r_init, atol=1e-8)
-    np.testing.assert_allclose(p, p_init, atol=1e-8)
+    np.testing.assert_allclose(r, r_init, atol=1e-6)
+    np.testing.assert_allclose(p, p_init, atol=1e-6)
 
