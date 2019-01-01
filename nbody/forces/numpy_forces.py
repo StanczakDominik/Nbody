dac17092e4a891b6ad76d5c3745a2f2433a784ef
Dominik Stańczak <stanczakdominik@gmail.com>
2019-01-01 14:00:36 +0100

    add potential tests

--- nbody/forces/numpy_forces.py
+++ nbody/forces/numpy_forces.py
@@ -20,6 +20,10 @@ def lenard_jones_force(r, well_depth=1, diameter=1, *args, **kwargs):
     return -12 * well_depth * ((diameter / r) ** 11 - (diameter / r) ** 5)
 
 
+def lenard_jones_potential(r, well_depth=1, diameter=1):
+    return 4 * well_depth * ((diameter / r) ** 12 - (diameter / r) ** 6)
+
+
 def calculate_forces(
     r: np.ndarray, force_law=lenard_jones_force, out=None, *args, **kwargs
 ):
@@ -59,7 +63,36 @@ def calculate_forces(
         return forces.sum(axis=1)
 
 
-def lenard_jones_potential(r1, r2, well_depth=1, diameter=1):
-    r = r1 - r2
-    norm_r = np.linalg.norm(r)
-    return 4 * well_depth * ((diameter / norm_r) ** 12 - (diameter / norm_r) ** 6)
+def calculate_potentials(
+    r: np.ndarray, potential_law=lenard_jones_potential, out=None, *args, **kwargs
+):
+    """
+
+    Parameters
+    ----------
+    r :
+        Nx3 array of particle positions
+    args :
+    kwargs :
+        passed along to the force law
+
+    Notes
+    -----
+    1. get a NxNx3 antisymmetric (upper triangular) matrix of vector distances
+    2a. from 1 get a normalized NxNx3 antisymmetric (matrix of direction vectors
+    2b. from 1 get a NxN (upper triangular due to symmetry) matrix of scalar distances
+    3b. get a NxN matrix of force magnitudes (reshapable to
+    3. multiply 2a by 3b to get forces
+    4. update existing force matrix
+
+    Returns
+    -------
+
+    """
+    # TODO optimize with upper triangular matrix
+    N = r.shape[0]
+    rij = r.reshape(N, 1, 3) - r.reshape(1, N, 3)
+    distances_ij = np.sqrt(np.sum(rij ** 2, axis=2, keepdims=True))
+    distances_ij[np.arange(N), np.arange(N), :] = np.inf
+    potentials = potential_law(distances_ij, *args, **kwargs)
+    return potentials.sum() / 2
--- nbody/forces/tests/test_numpy_forces.py
+++ nbody/forces/tests/test_numpy_forces.py
@@ -1,29 +1,32 @@
 import numpy as np
 import pytest
 
-from nbody.forces.numpy_forces import calculate_forces
+from nbody.forces.numpy_forces import calculate_forces, calculate_potentials
 from hypothesis import given
 from hypothesis.extra.numpy import arrays
 from hypothesis.strategies import floats
 
 
 @pytest.mark.parametrize(
-    "r2, expected_force_on_1",
+    "r2, expected_force_on_1, expected_potential_on_1",
     [
-        [0.5, 24192],
-        [1, 0],
-        [2, -0.369_140_625],  # regression test
-        [-1, 0],
-        [-2, 0.369_140_625],  # regression test
-        [-0.5, -24192],
+        [0.5, 24192, 16128],
+        [1, 0, 0],
+        [2, -0.369_140_625, -0.061_523_437_5],  # regression test
+        [-1, 0, 0],
+        [-2, 0.369_140_625, -0.061_523_437_5],  # regression test
+        [-0.5, -24192, 16128],
     ],
 )
-def test_lenard_jones_1d(r2, expected_force_on_1):
+def test_lenard_jones_1d(r2, expected_force_on_1, expected_potential_on_1):
     r1 = 0
     r = np.vstack([(r1, 0, 0), (r2, 0, 0)])
     forces = calculate_forces(r)
+    potential_energy = calculate_potentials(r)
     # check force value
     np.testing.assert_allclose(forces[0, 0], expected_force_on_1)
+    print(potential_energy)
+    np.testing.assert_allclose(potential_energy, expected_potential_on_1)
 
     # check reciprocity (Newton's third law)
     np.testing.assert_allclose(forces[1], -forces[0])
