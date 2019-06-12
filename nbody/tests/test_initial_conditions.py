7e1067367fc363ad9a3a36443970f8adf25aad95
Dominik Stańczak <stanczakdominik@gmail.com>
2019-06-12 07:24:20 +0200

    fix maxwell distro test

--- nbody/tests/test_initial_conditions.py
+++ nbody/tests/test_initial_conditions.py
@@ -25,13 +25,15 @@ def test_openpmd_file(openpmd_file):
 
 
 def test_maxwellian_momenta():
-    N = 1000
+    N = 1000000
     m = np.ones((N, 1))
     T = 1
     p = maxwellian_momenta(T, m)
-    assert_allclose(p.mean(), 0, atol=1e-12)
-    assert_allclose(p.var(), k_B * T / 1, atol=1e-12)
-    assert_allclose(p.var(axis=0), k_B * T / 1, atol=1e-12)
+    atol = 1e-2
+    rtol = 1e-6
+    assert_allclose(0, p.mean(), atol=atol, rtol=rtol)
+    assert_allclose(p.var(), k_B * T / 1, atol=atol, rtol=rtol)
+    assert_allclose(p.var(axis=0), k_B * T / 1, atol=atol, rtol=rtol)
 
 def test_xyz():
     save_xyz("/tmp/test.xyz", np.random.random((10, 3)) * 10, "Ar")
